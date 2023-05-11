dc9d43b43117ac81f2a181fe62d65f3bcc7dd6e5
NKIyuki <u.y0701@icloud.com>
2023-05-11 13:21:48 +0000

    管理者

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,10 +2,14 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  
-  
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+
   private
-  
+
 end
