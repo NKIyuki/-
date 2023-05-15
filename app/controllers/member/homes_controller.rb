ab1152afea9b8c3c0722ddc25cf9f9b226ad4153
NKIyuki <u.y0701@icloud.com>
2023-05-15 13:00:14 +0000

    レイアウト

--- app/controllers/member/homes_controller.rb
+++ app/controllers/member/homes_controller.rb
@@ -1,8 +1,7 @@
 class Member::HomesController < ApplicationController
 
   def top
-    @posts = Post.all
-    @post = Post.order('id DESC').limit(3)
+    @posts = Post.all.order('id DESC').limit(4)
   end
 
 end
